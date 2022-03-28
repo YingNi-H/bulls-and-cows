fea7a0fc32cc0b66cf3d7bc2217c36a489f58102
YingNi-H <yhua308@aucklanduni.ac.nz>
2022-03-29 09:35:58 +1300

    Very first little try out.

--- src/PlayerOnlyGuess.java
+++ src/PlayerOnlyGuess.java
@@ -8,17 +8,17 @@ public class PlayerOnlyGuess {
     protected int numberOfCows=0;
 
 
-
-
-
-
-
     public void compare(){
         computerSecreteNumber= new int[4];
-        computerSecreteNumber[0]= (int)(Math.random()*10);
-        computerSecreteNumber[1]= (int)(Math.random()*10);
-        computerSecreteNumber[2]= (int)(Math.random()*10);
-        computerSecreteNumber[3]= (int)(Math.random()*10);
+
+        /*while(computerSecreteNumber[0]!=computerSecreteNumber[1]&&computerSecreteNumber[0]!=computerSecreteNumber[2]&&
+                    computerSecreteNumber[0]!=computerSecreteNumber[3]&&computerSecreteNumber[1]!=computerSecreteNumber[2]&&
+                    computerSecreteNumber[1]!=computerSecreteNumber[3]&&computerSecreteNumber[2]!=computerSecreteNumber[3]){*/
+        for (int i = 0; i < 4 ; i++) {
+           computerSecreteNumber[i]= (int)(Math.random()*10);
+
+        }
+
 
         System.out.println("0: "+computerSecreteNumber[0]+" 1: "+computerSecreteNumber[1]+" 2: "+computerSecreteNumber[2]+" 3: "+computerSecreteNumber[3]);
         System.out.println(computerSecreteNumber[0]+""+computerSecreteNumber[1]+""+computerSecreteNumber[2]+""+computerSecreteNumber[3]);
@@ -27,36 +27,59 @@ public class PlayerOnlyGuess {
         System.out.println("Please enter your guess: ");
         playerGuess= new int[4];
         String playerGuessstr= Keyboard.readInput();
-        playerGuess[0]= Integer.parseInt(playerGuessstr.valueOf(0));
-        playerGuess[1]= Integer.parseInt(playerGuessstr.valueOf(1));
-        playerGuess[2]= Integer.parseInt(playerGuessstr.valueOf(2));
-        playerGuess[3]= Integer.parseInt(playerGuessstr.valueOf(3));
-
-
-
         for (int i = 0; i < 4; i++) {
-            if(playerGuess[i]==computerSecreteNumber[i]){
-                numberOfBulls++;
-            }
+            playerGuess[i]= Integer.parseInt(playerGuessstr.valueOf(i));
         }
-        if(playerGuess[0]!=computerSecreteNumber[0]&&playerGuess[0]==computerSecreteNumber[1]
-                ||playerGuess[0]==computerSecreteNumber[2]||playerGuess[0]==computerSecreteNumber[3]){
+        //System.out.println(playerGuess.length);
+
+
+//        for (int i = 0; i < 4; i++) {
+//            if(playerGuess[i]==computerSecreteNumber[i]){
+//                numberOfBulls++;
+//            }
+//
+//        }
+        if((playerGuess[1]==computerSecreteNumber[1])){
+            numberOfBulls=numberOfBulls+1;
+            System.out.println("playerGuess[1]==computerSecreteNumber[1]");
+        }
+        if((playerGuess[0]==computerSecreteNumber[0])){
+            numberOfBulls=numberOfBulls+1;
+            System.out.println("playerGuess[0]==computerSecreteNumber[0]");
+        }
+
+        if((playerGuess[2]==computerSecreteNumber[2])){
+            numberOfBulls=numberOfBulls+1;
+            System.out.println("playerGuess[2]==computerSecreteNumber[2]");
+        }
+        if((playerGuess[3]==computerSecreteNumber[3])){
+            numberOfBulls=numberOfBulls+1;
+            System.out.println("playerGuess[3]==computerSecreteNumber[3]");
+        }
+
+
+
+        /*if((playerGuess[0]!=computerSecreteNumber[0])&&((playerGuess[0]==computerSecreteNumber[1])
+                ||(playerGuess[0]==computerSecreteNumber[2])||(playerGuess[0]==computerSecreteNumber[3]))){
             numberOfCows++;
         }
-        if(playerGuess[1]!=computerSecreteNumber[1]&&playerGuess[1]==computerSecreteNumber[2]
-                ||playerGuess[1]==computerSecreteNumber[3]){
+        if((playerGuess[1]!=computerSecreteNumber[1])&&((playerGuess[1]==computerSecreteNumber[2])
+                ||(playerGuess[1]==computerSecreteNumber[3]))){
             numberOfCows++;
         }
-        if(playerGuess[2]!=computerSecreteNumber[2]&&playerGuess[2]==computerSecreteNumber[3]){
+        if((playerGuess[2]!=computerSecreteNumber[2])&&(playerGuess[2]==computerSecreteNumber[3])
+                || (playerGuess[1]==computerSecreteNumber[2])){
             numberOfCows++;
         }
-        if(playerGuess[3]!=computerSecreteNumber[3]&&playerGuess[3]==computerSecreteNumber[4]){
+        if((playerGuess[3]!=computerSecreteNumber[3])&&((playerGuess[3]==computerSecreteNumber[1])
+                ||(playerGuess[3]==computerSecreteNumber[2])||(playerGuess[3]==computerSecreteNumber[0]))){
             numberOfCows++;
         }
+*/
 
 
         System.out.println("Your guess: "+playerGuessstr);
-        System.out.println("Results: "+numberOfBulls+" bulls and "+numberOfCows+"cows");
+        System.out.println("Results: "+numberOfBulls+" bulls and "+numberOfCows+" cows");
 
 
     }
