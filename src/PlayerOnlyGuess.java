e2e7c256d50da043227df87616b20f4156052b3b
YingNi-H <yhua308@aucklanduni.ac.nz>
2022-11-20 13:07:27 +1300

    Tidy PlayerOnlyGuess

--- src/PlayerOnlyGuess.java
+++ src/PlayerOnlyGuess.java
@@ -14,7 +14,6 @@ public class PlayerOnlyGuess {
     public void start() {
         int[] computerSecreteNumber = setComputerSecreteNumber();
         game(computerSecreteNumber);
-
     }
 
 
@@ -23,21 +22,14 @@ public class PlayerOnlyGuess {
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
-
         Collections.shuffle(list);
-
         int[] computerSecreteNumber = new int[4];
         for (int i = 0; i < 4; i++) {
             computerSecreteNumber[i] = list.get(i);
         }
-
-//        for(Integer element : computerSecreteNumber){
-//            System.out.println(element);
-//        }
-
         return computerSecreteNumber;
-
     }
+
 
     public int[] getPlayerGuess() {
         String playerGuessstr = "";
@@ -45,33 +37,25 @@ public class PlayerOnlyGuess {
         while (!valid) {
             try {
                 playerGuessstr = Keyboard.readInput();
-
                 if ((playerGuessstr.length()) != max_digit) {
                     throw new StringIndexOutOfBoundsException(max_digit + " digits numbers!");
                 }
-
                 for (int i = 0; i < max_digit; i++) {
                     for (int j = 0; j < max_digit; j++) {
                         if ((i != j) && playerGuessstr.charAt(i) == playerGuessstr.charAt(j)) {
-
                             throw new SameNumberException("No same numbers!");
                         }
                     }
-
                 }
                 valid = true;
-
             } catch (NumberFormatException | StringIndexOutOfBoundsException | SameNumberException e) {
                 System.out.println("Error: " + e.getMessage() + " Try a valid number >> ");
-
             }
         }
         int[] playerGuess = new int[max_digit];
         for (int j = 0; j < max_digit; j++) {
             playerGuess[j] = Integer.parseInt(playerGuessstr.substring(j, j + 1));
-
         }
-
         return playerGuess;
     }
 
@@ -91,18 +75,17 @@ public class PlayerOnlyGuess {
                 System.out.println("Congratulation! You win!");
                 break;
             }
-
         }
         if (!win) {
             System.out.println("Time's up. You didn't get it!");
         }
-
-
     }
+
 
     public boolean winPlayer(int[] playerGuess, int[] computerSecreteNumber) {
         return Arrays.equals(playerGuess, computerSecreteNumber);
     }
+
 
     public void printEachResult(int counter, int bulls, int cows, int[] playerGuess) {
         System.out.println("Turn " + counter + " -Your guess: " + playerGuess[0] + "" + playerGuess[1] + "" + playerGuess[2] + "" + playerGuess[3]);
@@ -134,6 +117,4 @@ public class PlayerOnlyGuess {
         }
         return cows;
     }
-
-
 }
